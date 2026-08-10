fb30139c17d335b38596c3c8d5dbce36f8b4062b
ionut <rautuionut7@gmail.com>
2026-08-11 02:33:32 +0300

    created the MakeTodo class for creating todo objects and added a css reset file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import MakeTodo from './logic/todos.js'
+import './styles/style.css'
+
+
+const todo1 = new MakeTodo('nigga', 'this is a nigga todo', '11.08.2026', 'medium')
+const todo2 = new MakeTodo('nigga', 'this is a nigga todo', '11.08.2026', 'high')
+console.log(todo1, todo2)
