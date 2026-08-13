8cd1ee0bab6e8b906f815cb42e817f998f7bcf91
ionut <rautuionut7@gmail.com>
2026-08-13 03:33:08 +0300

    created the createList function that creates and appends lists to the dom and also initialized the default list using the load event

--- src/dom/events.js
+++ src/dom/events.js
@@ -1,14 +1,25 @@
 import state from '../logic/state.js'
 import elements from './elements.js'
 
+function createList(name) {
+  state.addList(name)
+
+  const container = document.createElement('div')
+  
+  container.appendChild(document.createElement('h2'))
+  container.firstChild.textContent = name
+  container.classList.add('list')
+  document.querySelector('.sidebar').appendChild(container)
+
+  elements.addInput.value = ''
+}
+
 document.body.addEventListener('click', (e) => {
   if (e.target.classList.contains('add-btn')) {
-    state.addList(elements.addInput.value)
-
-    const container = document.createElement('div')
-  
-    container.appendChild(document.createElement('h2'))
-    container.firstChild.textContent = elements.addInput.value
-    document.querySelector('.sidebar').appendChild(container)
+    createList(elements.addInput.value)
   }
+})
+
+window.addEventListener('load', (e) => {
+  createList('default ')
 })
